7c23ab9bade19144e3f86eec14279dce18ae4379
Freedom170691 <Freedom170691@gmail.com>
2022-10-23 12:51:55 +0300

    initial commit

---
+++ frontend/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from "./App";
+
+const app = (
+  // <Provider store={store}>
+  //   <ThemeProvider theme={theme}>
+      // <Router history={history}>
+        <App/>
+      // </Router>
+    // </ThemeProvider>*/
+  // </Provider>
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(app);
